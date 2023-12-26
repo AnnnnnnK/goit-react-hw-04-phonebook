ad825d4707fce39bc20757be43e0eebfca4d32d9
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2023-12-26 14:53:15 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import React from 'react';
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) ?? '';
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
   });
   const [filter, setFilter] = useState('');
 
